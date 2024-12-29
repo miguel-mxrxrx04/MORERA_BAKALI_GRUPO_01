6456a1335caa545cf9d15a9248234ca27bb843b2
miguel-mxrxrx04 <miguel.morera@alumnos.upm.es>
2024-12-29 20:15:21 +0100

    initial setup

---
+++ core.py
@@ -0,0 +1,35 @@
+import pandas as pd
+
+#Modulo 1
+
+#Modulo 2
+def classify_subredit(text):
+    pass
+
+#Modulo 3
+def find_subredit_mentions(text: str):
+    pass
+
+def url_extraction(text: str):
+    pass
+
+def phone_number_extracion(text: str):
+    pass
+
+def dates_extraction(text: str):
+    pass
+
+def code_extraction(text:str):
+    pass
+
+#Modulo 4
+def sentiment_analysis(text: str):
+    pass
+
+# Modulo 5
+def post_summarisation(text: str):
+    pass
+
+#Modulo 6
+def texts_distance(text1: str, text2: str):
+    pass 
